643423be03d7e89280e36817735201b6fce57324
Josmar Dias <josmarnet@gmail.com>
2012-08-11 18:43:54 -0300

    First layout workaround
    It's ugly, i know. I'll try to get simpler and cleaner.

--- client/main.js
+++ client/main.js
@@ -1,16 +1,37 @@
 
 var AppRouter = Backbone.Router.extend({
     routes: {
-        'site/login' : 'login'
+        '': 'index',
+        'security': 'security',
+        'history': 'history',
+        'logout': 'logout'
     },
-    login: function () {
-        console.log('login');
+    logout: function () {
+        console.log('logout');
+    },
+    index: function () {
+        console.log('index');
+        Session.set('content', 'index');
+    },
+    security: function () {
+        console.log('security');
+        Session.set('content', 'security');
+    },
+    history: function () {
+        console.log('history');
+        Session.set('content', 'history');
     }
 });
 
 Router = new AppRouter;
 
+Template.content.content_is = function (data) {
+    return Session.equals('content', data);
+};
+
 Meteor.startup(function (){
-    Backbone.history.start({pushState: true});
+    Backbone.history.start({
+        pushState: true
+    });
 });
 
