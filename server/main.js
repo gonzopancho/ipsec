16b7865411e0c870b6ea459009d1d3de9f61b4cb
Josmar Dias <josmarnet@gmail.com>
2014-01-03 15:51:30 -0200

    adding facts package

--- server/main.js
+++ server/main.js
@@ -2,6 +2,11 @@ Meteor.publish('ips', function () {
     return Ips.find({user: this.userId});
 });
 
+Facts.setUserIdFilter(function (userId) {
+  var user = Meteor.users.findOne(userId);
+  return user && user.admin;
+});
+
 Meteor.startup(function () {
     // code to run on server at startup
 });
